73e8e8b9cb0629f8b56b03916672d2e14764cf6c
Tyapor718 <katepsupport@mail.com>
2023-04-15 15:13:53 +0400

    Массива вещественных чисел

---
+++ ex01/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача 1: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+int ReadInt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+double[,] CreateArray(int masRows, int masColumns)
+{
+    double[,] array = new double[masRows, masColumns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.NextDouble()*100;
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]:F2}  ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+
+
+int rowMas = ReadInt("Количество строк массива  >");
+int columnMas = ReadInt("Количесвто столбцов массива >");
+double[,] massive = CreateArray(rowMas, columnMas);
+PrintArray(massive);
+
